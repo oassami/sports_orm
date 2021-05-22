d3451bb8135ccd99705c33589e51ba53c8e7153a
oassami <76979230+oassami@users.noreply.github.com>
2021-05-22 11:03:18 -0700

    Query: All womens' leagues

--- leagues/views.py
+++ leagues/views.py
@@ -4,11 +4,11 @@ from .models import League, Team, Player
 from . import team_maker
 
 def index(request):
-	# Query 1 : All baseball leagues
+	# Query 1 : All womens' leagues
 	context = {
-		"q_title": "All baseball leagues",
+		"q_title": "All womens' leagues",
 		# "leagues": League.objects.all(),
-		"leagues": League.objects.filter(sport__contains='Baseball'),
+		"leagues": League.objects.filter(name__contains="womens'"),
 		# "teams": Team.objects.all(),
 		# "players": Player.objects.all(),
 	}
